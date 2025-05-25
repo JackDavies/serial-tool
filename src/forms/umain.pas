520187d07517d0667e1173df01dd0892a1004966
JackDavies <28daviesja@gmail.com>
2025-05-25 11:30:00 +0100

    Added ports list Combo box
    Added res/images to repo

--- src/forms/umain.pas
+++ src/forms/umain.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  Buttons, Menus, synaser;
+  Buttons, Menus, synaser, FileUtil;
 
 type
 
@@ -16,7 +16,7 @@ type
     BottomPanel: TPanel;
     Button1: TButton;
     BaudRateCombo: TComboBox;
-    DeviceComboBox: TComboBox;
+    PortsComboBox: TComboBox;
     ConnectButton: TButton;
     InputTypeComboBox1: TComboBox;
     OutputSettingsPanel: TPanel;
@@ -25,10 +25,14 @@ type
     InputTypeComboBox: TComboBox;
     InputEdit: TEdit;
     OutputMemo: TMemo;
+    RefreshPortsButton: TSpeedButton;
     TopPanel: TPanel;
     procedure Button1Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure RefreshPortsButtonClick(Sender: TObject);
   private
-
+    procedure PopulatePortsList();
+    function GetPortNames() : TStringList;
   public
 
   end;
@@ -41,6 +45,111 @@ implementation
 {$R *.lfm}
 
 { TMainForm }
+
+{$REGION 'Private Methods'}
+
+{ Adapted from synaser.GetSerialPortNames: string; }
+function TMainForm.GetPortNames() : TStringList;
+const
+  ATTR = {$IFDEF POSIX}$7FFFFFFF{$ELSE}$FFFFFFFF{$ENDIF};
+var
+  sr : TSearchRec;
+  portName : string;
+  names : TStringList;
+begin
+{$DEFINE IncttyACM}
+{$DEFINE IncttyAM}
+{$DEFINE IncttyS}
+{$DEFINE IncttyUSB}
+
+  names := TStringList.Create();
+
+{$IFDEF IncttyACM}
+
+  if FindFirst('/dev/ttyACM*', ATTR, sr) = 0 then begin
+    repeat
+      if (sr.Attr and ATTR) = Sr.Attr then begin
+        portName := '/dev/' + sr.Name;
+        names.Add(portName);
+      end;
+    until FindNext(sr) <> 0;
+  end;
+  FindClose(sr);
+
+{$ENDIF}
+
+{$IFDEF IncttyAM}
+
+  if FindFirst('/dev/ttyAM*', ATTR, sr) = 0 then begin
+    repeat
+      if (sr.Attr and ATTR) = Sr.Attr then begin
+        portName := '/dev/' + sr.Name;
+        names.Add(portName);
+      end;
+    until FindNext(sr) <> 0;
+  end;
+  FindClose(sr);
+
+{$ENDIF}
+
+{$IFDEF IncttyS}
+
+  if FindFirst('/dev/ttyS*', ATTR, sr) = 0 then begin
+    repeat
+      if (sr.Attr and ATTR) = Sr.Attr then begin
+        portName := '/dev/' + sr.Name;
+        names.Add(portName);
+      end;
+    until FindNext(sr) <> 0;
+  end;
+  FindClose(sr);
+
+{$ENDIF}
+
+{$IFDEF IncttyUSB}
+
+  if FindFirst('/dev/ttyUSB*', ATTR, sr) = 0 then begin
+    repeat
+      if (sr.Attr and ATTR) = Sr.Attr then begin
+        portName := '/dev/' + sr.Name;
+        names.Add(portName);
+      end;
+    until FindNext(sr) <> 0;
+  end;
+  FindClose(sr);
+
+{$ENDIF}
+
+  Result := names;
+end;
+
+procedure TMainForm.PopulatePortsList();
+var
+  ttyFiles : TStringList;
+  tty : string;
+begin
+  PortsComboBox.Clear();
+  ttyFiles := GetPortNames();
+
+  for tty in ttyFiles do begin
+    PortsComboBox.Items.Add(tty);
+  end;
+end;
+
+{$ENDREGION}
+
+
+{$REGION 'Event Handlers'}
+
+procedure TMainForm.FormShow(Sender: TObject);
+begin
+  PopulatePortsList();
+end;
+
+procedure TMainForm.RefreshPortsButtonClick(Sender: TObject);
+begin
+  PopulatePortsList();
+end;
 
 procedure TMainForm.Button1Click(Sender: TObject);
 var
@@ -63,6 +172,8 @@ begin
 
   OutputMemo.Lines.Add(s);
 end;
+
+{$ENDREGION}
 
 end.
 
