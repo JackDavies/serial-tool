c38eaf73cdc84c316499ab0d21bdfd8a93ebf20e
JackDavies <28daviesja@gmail.com>
2025-05-29 23:19:58 +0100

    Implemented Send data in TSerialThread
    Added Send and SendTxData methods, Added destructor

--- src/units/userialcomms.pas
+++ src/units/userialcomms.pas
@@ -26,9 +26,13 @@ type
       _serial : TBlockSerial;
       _options : TSerialOptions;
       _rxData : TBytes;
+      _criticalSection : TRTLCriticalSection;
+      _sendTx : boolean;
+      _txData : TBytes;
     private
       procedure ErrorEvent();
       procedure RXEvent();
+      procedure SendTxData();
     public
       OnRX : TOnRXEvent;
       OnError : TOnErrorEvent;
@@ -36,8 +40,9 @@ type
       procedure Connect();
       procedure Disconnect();
       procedure Execute(); override;
+      procedure Send(data : TBytes);
       constructor Create(options : TSerialOptions); overload;
-
+      destructor Destroy(); override;
   end;
 
 implementation
@@ -47,11 +52,21 @@ implementation
 constructor TSerialThread.Create(options : TSerialOptions);
 begin
   inherited Create(true, DefaultStackSize);
-
+  _sendTx := false;
   _options := options;
 
   _serial := TBlockSerial.Create();
   _serial.LinuxLock := false;
+
+  InitCriticalSection(_criticalSection);
+end;
+
+destructor TSerialThread.Destroy();
+begin
+  DoneCriticalSection(_criticalSection);
+  FreeAndNil(_serial);
+  FreeAndNil(_options);
+  inherited Destroy;
 end;
 
 {$ENDREGION}
@@ -80,9 +95,50 @@ begin
   OnRX(_rxData);
 end;
 
+procedure TSerialThread.SendTxData();
+var
+  stream : TMemoryStream;
+begin
+  EnterCriticalSection(_criticalSection);
+
+  if _sendTx then begin
+    stream := TMemoryStream.Create;
+
+    stream.Write(_txData, Length(_txData));
+    stream.Position := 0;
+
+    _serial.SendStream(stream);
+
+    FreeAndNil(stream);
+
+    _sendTx := false;
+    SetLength(_txData, 0);
+  end;
+
+
+  LeaveCriticalSection(_criticalSection);
+end;
+
 {$ENDREGION}
 
 {$REGION 'Public Methods'}
+
+procedure TSerialThread.Send(data : TBytes);
+var
+  i : integer;
+begin
+  EnterCriticalSection(_criticalSection);
+
+  SetLength(_txData, Length(data));
+
+  for i := 0 to Length(data) - 1 do begin
+    _txData[i] := data[i];
+  end;
+
+  _sendTx := true;
+
+  LeaveCriticalSection(_criticalSection);
+end;
 
 procedure TSerialThread.Execute();
 var
@@ -93,11 +149,12 @@ begin
   _serial.Connect(_options.Port);
   _serial.Config(_options.Baud, 8, 'N', SB1, false, false);
 
-
   while(not Terminated) do begin
     if _serial.LastError <> 0 then begin
       Synchronize(@ErrorEvent);
     end;
+
+    SendTxData();
 
     if _serial.CanReadEx(1) then begin
       i := _serial.LastError;
